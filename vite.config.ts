463e0d7eeb6e88f21038528dbc96df077c03aa08
Shaikh Jubair <shaikhjubair@SHAIKHs-MacBook-Air.local>
2026-06-14 08:34:54 +0600

    fix: enforce lowercase zenith in base path and homepage URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Zenith/',
+    base: '/zenith/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
